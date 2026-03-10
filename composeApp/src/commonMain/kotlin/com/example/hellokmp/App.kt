97ec67d05f7a5781e2c80801160a03ec41e71ecb
ACER <arta.123140209@student.itera,ac.id>
2026-03-10 23:51:55 +0700

    initial commit

---
+++ composeApp/src/commonMain/kotlin/com/example/hellokmp/App.kt
@@ -0,0 +1,53 @@
+package com.example.hellokmp
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.jetbrains.compose.resources.painterResource
+
+import hellokmp.composeapp.generated.resources.Res
+import hellokmp.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text("Halo, Arta Eka!")
+                        Text("NIM: 123140209")
+                        Text("Platform: ${getPlatform().name}")
+                    }
+                }
+            }
+        }
+    }
+}
